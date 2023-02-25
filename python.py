63720eb1e1661ac0bcf6d0e5d8ccd3315947aa28
Goutham-algo <102314644+Goutham-algo@users.noreply.github.com>
2023-02-25 11:27:47 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
-print("hello")
+t = int(input())
+for i in range(t):
+  y,m,z,x = map(int,input().split())
+  try:
+    print((y*m)/(z*x))
+  except:
+    print("Don't enter the zero and negatives")
